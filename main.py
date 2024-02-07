c2d3710045c1ea8f76c6276d5a66d411db66e971
Karczel <92095164+Karczel@users.noreply.github.com>
2024-02-07 09:09:06 +0700

    a

--- main.py
+++ main.py
@@ -1,61 +1,88 @@
+"""Test code"""
+import unittest
 from q2 import*
 
-if __name__ == '__main__':
+class DecimalConverter(unittest.TestCase):
     """Test cases"""
-    print(dec_to_bin(7))
-    print(dec_to_base_y(7, 7))
-    print(dec_to_base_y(7, 3))
-    print(dec_to_base_y(7, 2))
-    print('---')
+    def test_tobin(self):
+        """to binary"""
+        print('Test cases')
+        self.assertEqual(111,dec_to_bin(7))
+        print('-------------------------------------------------------')
 
-    """In thr reverse direction"""
-    print(bin_to_dec(111))
-    print(bin_to_dec(110))
-    print(bin_to_dec(101001))
-    print(base_y_to_dec(1231231,4))
-    print(base_y_to_dec(10,7))
-    print(base_y_to_dec(21,3))
-    print('---')
-
-    """Hexadecimal"""
-    print(dec_to_hex(120))
-
-    """From Lab3 question:
-        Demonstrate how you can convert the 1873 base 10 
-        to the equivalent number 
-        in base 2, base 7, and base 8."""
-    print('\nTests from question 1:')
-    print(dec_to_bin(1873))
-    print(dec_to_base_y(1873, 7))
-    print(dec_to_base_y(1873, 8))
-    print('---')
-
-    """Question 4"""
-    l1 = 3410
-    l2 = 'FF7F'
-    l3 = '017037'
-    l4 = 1111101110110100
-    print('\n4 lines to solve: Decimal, Hexadecimal,Octal,Binary')
-    print("Assume 16-bit and two's complememt")
-    print('\nLine 1')
-    print(f'Decimal number is {l1}')
-    # print(f'Hexadecimal number is {}')
-    print(f'Octal number is {dec_to_base_y(l1,8)}')
-    print(f'Binary number is 0{dec_to_bin(l1)}')
-    print('\nLine 2')
-    # print(f'Decimal number is {}')
-    print(f'Hexadecimal number is {l2}')
-    # print(f'Octal number is {}')
-    # print(f'Binary number is {}')
-    print('\nLine 3')
-    print(f'Decimal number is {base_y_to_dec(17037,8)}')
-    # print(f'Hexadecimal number is {}')
-    print(f'Octal number is {l3}')
-    print(f'Binary number is 0{dec_to_bin(base_y_to_dec(17037,8))}')
-    print('\nLine 4')
-    print(f'Decimal number is {-bin_to_dec(10001001011)}')
-    # print(f'Hexadecimal number is {}')
-    print(f'Octal number is 77777 - {dec_to_base_y(bin_to_dec(10001001011),8)} +1 (leading 7s to indicate negative)')
-    print(f'Binary number is {l4}')
+    def test_tobase(self):
+        """to base y"""
+        self.assertEqual(10,dec_to_base_y(7, 7))
+        self.assertEqual(21,dec_to_base_y(7, 3))
+        self.assertEqual(dec_to_bin(7),dec_to_base_y(7, 2))
+        print('-------------------------------------------------------')
 
 
+    def test_todec(self):
+        """to decimal"""
+        print()
+        self.assertEqual(bin_to_dec(111),7)
+        self.assertEqual(bin_to_dec(110),6)
+        self.assertEqual(bin_to_dec(101001),41)
+        self.assertEqual(base_y_to_dec(1231231, 4),7021)
+        self.assertEqual(base_y_to_dec(10, 7),7)
+        self.assertEqual(base_y_to_dec(21, 3),7)
+        print('-------------------------------------------------------')
+
+
+    def test_hexadecimal(self):
+        """Hexadecimal"""
+        self.assertEqual(78,int(dec_to_hex(120)))
+        self.assertEqual(893247,hex_to_dec('DA13F'))
+        print('-------------------------------------------------------')
+
+    def test_complement(self):
+        """check if value is negative (leading 1, 7, F, etc)
+        and if it is, return complement value"""
+
+    def test_lab3_question2(self):
+        """From Lab3 question:
+                Demonstrate how you can convert the 1873 base 10
+                to the equivalent number
+                in base 2, base 7, and base 8."""
+        print('\nTests from question 1:')
+        print(dec_to_bin(1873))
+        print(dec_to_base_y(1873, 7))
+        print(dec_to_base_y(1873, 8))
+        print('-------------------------------------------------------')
+
+
+    def test_lab3_question4(self):
+        """Question 4"""
+        l1 = 3410
+        l2 = 'FF7F'
+        l3 = '017037'
+        l4 = '1111 1011 1011 0100'
+        print('\n4 lines to solve: Decimal, Hexadecimal,Octal,Binary')
+        print("Assume 16-bit and two's complememt")
+        print('\nLine 1')
+        print(f'Decimal number is {l1}')
+        print(f'Hexadecimal number is {dec_to_hex(l1)}')
+        print(f'Octal number is {dec_to_base_y(l1, 8)}')
+        print(f'Binary number is 0{dec_to_bin(l1)}')
+        print('\nLine 2')
+        # print(f'Decimal number is -{hex_to_dec(hex_complement(l2))}')
+        print(f'Hexadecimal number is {l2}')
+        # print(f'Octal number is {dec_to_base_y(hex_to_dec(hex_complement(l2)), 8)}')
+        # print(f'Binary number is 1111 1111 1111 1111-{dec_to_bin(hex_to_dec(hex_complement(l2)))} + 1')
+        print('\nLine 3')
+        print(f'Decimal number is {base_y_to_dec(17037, 8)}')
+        print(f'Hexadecimal number is {dec_to_hex(base_y_to_dec(17037, 8))}')
+        print(f'Octal number is {l3}')
+        print(f'Binary number is 0{dec_to_bin(base_y_to_dec(17037, 8))}')
+        print('\nLine 4')
+        print(f'Decimal number is {-bin_to_dec(10001001011)}')
+        print(f'Hexadecimal number is FFFF FFFF FFFF FFFF- {dec_to_hex(bin_to_dec(10001001011))} + 1 (leading Fs to indicate negative)')
+        print(
+            f'Octal number is 7777 7777 7777 7777 - {dec_to_base_y(bin_to_dec(10001001011), 8)} +1 (leading 7s to indicate negative)')
+        print(f'Binary number is {l4}')
+        print('-------------------------------------------------------')
+
+
+if __name__=='__main__':
+    unittest.main()
--- q2.py
+++ q2.py
@@ -26,16 +26,51 @@ def base_y_to_dec(x: int, y: int):
         return 0
     return x % 10 + y * base_y_to_dec(x // 10, y)
 
+# --- Code Beyond this point just for fun, so some are left unfinished ---
 
 # Hex: 0123456789abcdef
 
-def hex_filter(x):
-    characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                   'A', 'B', 'C', 'D', 'E', 'F']
+
+def hex_filter(x):
     return characters[x]
 
+def hex_revert(x, lst):
+    if x == lst[0]:
+        return 0
+    return 1 + hex_revert(x, lst[1:])
 
 def dec_to_hex(x):
-    if x == 0:
-        return '0'
+    if x < 16:
+        return hex_filter(x)
     return dec_to_hex(x // 16) + hex_filter(x % 16)
+
+def hex_to_dec(x):
+    if len(x) == 1:
+        return hex_revert(x,characters)
+    return hex_revert(x[-1], characters) + 16 * hex_to_dec(x[:-1])
+
+# # complement system
+# def is_negative(x,y):
+#     """input non-decimal numbers
+#     :param x: int, str
+#     :param y: int
+#     :return bool
+#     """
+#     if y > 10:
+#         # code
+#     # code for base < 10
+
+# def bin_complement(x):
+#     """return negative value equivalent in 2's complement system"""
+#
+# def base_y_complement(x,y):
+#     """return negative value equivalent in y's complement system"""
+#
+# def hex_complement(x):
+#     """return negative value equivalent in Hexadecimal complement system"""
+
+# def bit16(x,y):
+#     """take input and turn it into 16 bit"""
+#     if
